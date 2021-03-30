98b71ca11f3ab9d78c70e1187cdca48de0d1b91e
amzrk2 <amzrk2@outlook.com>
2021-03-30 16:07:29 +0800

    fix: vercel api build script compatibility

--- utils/build.js
+++ utils/build.js
@@ -1,7 +1,15 @@
 require('./env')();
+const fs = require('fs');
+const path = require('path');
 
 // timer
 const startTime = Date.now();
+
+// create empty `dist` folder for vercel-build
+const dist = path.resolve(__dirname, '../dist');
+if (!fs.existsSync(dist)) {
+  fs.mkdirSync(dist);
+}
 
 // builders
 const buildGeoLite = require('./build-geolite');
