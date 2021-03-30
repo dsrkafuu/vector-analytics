75b3cc26250a606500336beb4229289ba68547b9
amzrk2 <amzrk2@outlook.com>
2021-03-30 16:18:49 +0800

    Revert "fix: vercel api build script compatibility"
    This reverts commit fca89767eba9d511f0d064b0c217b82f739026d1 and 98b71ca11f3ab9d78c70e1187cdca48de0d1b91e.

--- utils/build.js
+++ utils/build.js
@@ -1,17 +1,7 @@
 require('./env')();
-const fs = require('fs');
-const path = require('path');
 
 // timer
 const startTime = Date.now();
-
-// create empty `dist` folder for vercel-build
-const dist = path.resolve(__dirname, '../dist');
-if (!fs.existsSync(dist)) {
-  fs.mkdirSync(dist);
-}
-const meta = path.resolve(dist, './metadata');
-fs.writeFileSync(meta, `[AOFUJI] BUILD DATE ${new Date().toJSON()}`);
 
 // builders
 const buildGeoLite = require('./build-geolite');
