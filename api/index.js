c0b22371b4f79b7e1cf76bd94aa25d1fc816c8e3
dsrkafuu <dsrkafuu@outlook.com>
2021-04-01 19:43:03 +0800

    fix: remove api server port env

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 /*! aofuji-analytics | DSRKafuU (https://dsrkafuu.su) | Copyright (c) MIT License */
 
 require('../utils/env')();
+const production = process.env.NODE_ENV === 'production';
 
 const app = require('../src/api');
 const serverless = require('serverless-http');
@@ -8,19 +9,16 @@ const serverless = require('serverless-http');
 // normal
 if (!process.env.SERVERLESS) {
   // static server in production
-  if (process.env.NODE_ENV === 'production') {
+  if (production) {
     const { static: serveStatic } = require('express');
     const history = require('connect-history-api-fallback');
     app.use(history());
     app.use(serveStatic('dist'));
   }
   // port listening
-  const port =
-    process.env.NODE_ENV === 'production'
-      ? process.env.SERVER_PORT || 3000
-      : process.env.SERVER_API_PORT || 3001;
+  const port = production ? process.env.SERVER_PORT || 3000 : 3022;
   app.listen(port, () => {
-    console.log(`[aofuji] listening at http://localhost:${port}`);
+    console.log(`[aofuji]${production ? '' : 'dev api'} listening at http://localhost:${port}`);
   });
 }
 
